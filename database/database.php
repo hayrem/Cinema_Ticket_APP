c771aeaaa9029824966b07102b021d102908d8ca
sreyrea <hansreyrea2222@gmail.com>
2023-01-24 11:22:20 +0700

    [database and php] connect database with php

---
+++ database/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$hostname = "localhost";
+$database = "cinema_ticket_app";
+$username = "root";
+$password = "";
+
+$dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+$connection = new PDO($dsn, $username, $password);
