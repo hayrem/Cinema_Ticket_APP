908b4c8e7a32f943e1c55f6bcd88421608f21f2f
sreyrea <hansreyrea2222@gmail.com>
2023-01-24 14:22:53 +0700

    [CP-15] create function insert data into table users

---
+++ models/post.model.php
@@ -0,0 +1,15 @@
+<?php
+
+function createUser(string $userName, string $email,string $password, string $confirmPassword ) : bool
+{
+    global $connection;
+    $statement = $connection->prepare("insert into users (user_name, email,password, confirm_password) values (:user_name, :email,:password, :confirm_password)");
+    $statement->execute([
+        ':user_name' => $userName,
+        ':email' => $email,
+        ':password' => $password,
+        ':confirm_password' => $confirmPassword
+    ]);
+
+    return $statement->rowCount() > 0;
+}
