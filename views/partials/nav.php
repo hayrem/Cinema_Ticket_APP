965eae20f122d130ee8588f0db9e3d17f693bd39
Student <NongPhloeut>
2023-01-24 17:43:29 +0700

    [CP-27] display form dialog from navbar

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -25,8 +25,8 @@ require "head.php"
 
                                     <div class="modal-body d-flex mt-dialog">
 
-                                        <div class="col-md-6 col-lg-5 d-none d-md-block ">
-                                            <img src="https://images-na.ssl-images-amazon.com/images/I/6138vGyCo2L._AC_UL900_SR615,900_.jpg"  alt="login form" class="img-fluid" />
+                                        <div class="bg-img">
+                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/94ab2ad3-e0d4-4764-85ad-873af742bd4a/ddar2mb-683c8e32-65e4-4bd6-b9e1-c68423cf01a7.png/v1/fill/w_1600,h_2437,q_80,strp/marvel_s_new_avengers_movie_poster_by_arkhamnatic_ddar2mb-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk0YWIyYWQzLWUwZDQtNDc2NC04NWFkLTg3M2FmNzQyYmQ0YVwvZGRhcjJtYi02ODNjOGUzMi02NWU0LTRiZDYtYjllMS1jNjg0MjNjZjAxYTcucG5nIiwiaGVpZ2h0IjoiPD0yNDM3Iiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvOTRhYjJhZDMtZTBkNC00NzY0LTg1YWQtODczYWY3NDJiZDRhXC9hcmtoYW1uYXRpYy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.AtV__jHLgSoG2HR6Rkt5D7kERV_ekVZxxZ7yFgDKH68" alt="login form" class="img-fluid" />
                                         </div>
                                         <div class="col-md-6 col-lg-7 ">
                                             <div class="card-body p-4 p-lg-5 text-white">
@@ -37,12 +37,12 @@ require "head.php"
                                                 <form>
 
                                                     <div class="form-outline mb-4">
-                                                        <label class="form-label d-flex justify-content-start" >Email address</label>
+                                                        <label class="form-label d-flex justify-content-start">Email address</label>
                                                         <input type="email" id="form2Example17" class="form-control form-control-lg" />
                                                     </div>
 
                                                     <div class="form-outline mb-4">
-                                                        <label class="form-label d-flex justify-content-start " >Password</label>
+                                                        <label class="form-label d-flex justify-content-start ">Password</label>
                                                         <input type="password" id="form2Example27" class="form-control form-control-lg" />
                                                     </div>
 
@@ -71,10 +71,69 @@ require "head.php"
 
                         <!-- Modal login -->
 
-                        <a href="" class="text-white"> Register</a>
-                            
+                        <!-- Modal create account-->
 
-                    </div>
+                        <a href="" class="text-white" data-toggle="modal" data-target="#register"> Register</a>
+
+                        <div class="modal fade " id="register" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content  w-100px">
+
+                                    <div class="modal-body d-flex mt-dialog">
+
+                                        <div class="bg-img">
+                                            <img src="https://images-na.ssl-images-amazon.com/images/I/6138vGyCo2L._AC_UL900_SR615,900_.jpg" alt="login form"  width="100%" />
+                                        </div>
+                                        <div class="col-md-6 col-lg-7 ">
+                                            <div class="card-body p-4 p-lg-5 text-white">
+                                                <div class="d-flex  mb-4">
+                                                    <h3>Sign Up</h3>
+                                                </div>
+
+                                                <form>
+
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start">Username</label>
+                                                        <input type="text" id="form2Example17" class="form-control form-control-lg" />
+                                                    </div>
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start">Email address</label>
+                                                        <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                                    </div>
+
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start ">Password</label>
+                                                        <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                                    </div>
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start ">Comfirm Password</label>
+                                                        <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                                    </div>
+
+                                                    <!-- Checkbox -->
+                                                    <div class="form-check d-flex justify-content-start mb-3">
+                                                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                                                        <label class="form-check-label ms-3" for="form1Example3"> Remember me! </label>
+                                                    </div>
+
+                                                    <p class="mb-5 pb-lg-2 text-white d-flex justify-content-start" style="color: #393f81;">Have an account? <a href="./sign_up.view.php" style="color: #dc3545;">Sign In</a></p>
+                                                    <div class="pt-1 mb-4">
+                                                        <button class="form-control form-control-lg text-white border border-danger" style="background-color: rgb(159, 4, 4)" type="button">Sign Up</button>
+                                                    </div>
+
+
+                                                </form>
+
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal create account-->
                 </li>
                 <span class="material-symbols-outlined mt-4 ms-4" data-toggle="modal" data-target="#exampleModal">account_circle </span>
 
@@ -106,7 +165,7 @@ require "head.php"
                         </div>
                     </div>
                 </div>
-                 <!-- Modal manage account-->
+                <!-- Modal manage account-->
             </ul>
         </div>
 
