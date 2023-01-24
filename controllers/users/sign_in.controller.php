fd9587d83245806ac982ac813cade27a968b7515
hayya <remhay07@gmial.com>
2023-01-24 15:52:28 +0700

    [CP-21]move form to users folder

---
+++ controllers/users/sign_in.controller.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$username = ($_POST['username']);
+$password = ($_POST['password']);
+require ("models/sign_in.model.php");
+$user=getUseraccount($username,$password);
+
+if ($username===$user[0]['user_name'] && $password===$user[0]['password'])
+{
+
+    if(!empty($_POST["remember"])) 
+    {
+        setcookie ("username",$_POST["username"],time()+ 3600);
+        setcookie ("password",$_POST["password"],time()+ 3600);
+    } else {
+        setcookie("username","");
+        setcookie("password","");
+    };
+}
+?>
