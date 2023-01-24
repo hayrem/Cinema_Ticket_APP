6b6fad72bcb02252d668241b73f52db157fe7841
Student <NongPhloeut>
2023-01-24 16:11:55 +0700

    [CP-27] insert form create dialog into navbar

--- views/form/sign_in.view.php
+++ views/form/sign_in.view.php
@@ -2,59 +2,53 @@
 require("../partials/head.php");
 ?>
 
-    <div class="container py-5 h-100" >
-        <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col col-xl-10" >
-            <div class="card" style="border-radius: 1rem;" >
-            <div class="row g-0" style="background-color: #000000;">
-                <div class="col-md-6 col-lg-5 d-none d-md-block" >
-                    <img src="https://images-na.ssl-images-amazon.com/images/I/6138vGyCo2L._AC_UL900_SR615,900_.jpg"
-                        alt="login form" class="img-fluid"  />
-                </div>
-                <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                <div class="card-body p-4 p-lg-5 text-white">
 
-                    <form>
-
-                    <div class="d-flex align-items-center mb-3 pb-1" >
-                        <!-- <i class="fas fa-cubes fa-2x me-3" style="color: #ffffff;"></i> -->
-                        <span class="h1 fw-bold mb-0">Sign In</span>
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+            <div class="card" style="border-radius: 1rem;">
+                <div class="row g-0" style="background-color: #000000;">
+                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/6138vGyCo2L._AC_UL900_SR615,900_.jpg" alt="login form" class="img-fluid" />
                     </div>
+                    <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                        <div class="card-body p-4 p-lg-5 text-white">
 
-                    <p class="mb-5 pb-lg-2 text-white" style="color: #393f81;">Don't have an account? <a href="./sign_up.view.php"
-                        style="color: #dc3545;">Sign Up</a></p>
+                            <form>
 
-                    <div class="form-outline mb-4">
-                        <label class="form-label" for="form2Example17">Email address</label>
-                        <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                <div class="d-flex align-items-center mb-3 pb-1">
+                                    <span class="h1 fw-bold mb-0">Sign In</span>
+                                </div>
+
+                                <p class="mb-5 pb-lg-2 text-white" style="color: #393f81;">Don't have an account? <a href="./sign_up.view.php" style="color: #dc3545;">Sign Up</a></p>
+
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="form2Example17">Email address</label>
+                                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                </div>
+
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="form2Example27">Password</label>
+                                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                </div>
+
+                                <!-- Checkbox -->
+                                <div class="form-check d-flex justify-content-start mb-4">
+                                    <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                                    <label class="form-check-label" for="form1Example3"> Remember me! </label>
+                                </div>
+
+                                <div class="pt-1 mb-4">
+                                    <button class="form-control form-control-lg text-white border border-danger" style="background-color: rgb(159, 4, 4)" type="button">Login</button>
+                                </div>
+
+
+                            </form>
+
+                        </div>
                     </div>
-
-                    <div class="form-outline mb-4">
-                        <label class="form-label" for="form2Example27">Password</label>
-                        <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                    </div>
-
-                    <!-- Checkbox -->
-                    <div class="form-check d-flex justify-content-start mb-4">
-                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
-                        <label class="form-check-label" for="form1Example3"> Remember me! </label>
-                    </div>
-
-                    <div class="pt-1 mb-4">
-                        <button class="form-control form-control-lg text-white border border-danger" style="background-color: rgb(159, 4, 4)" type="button">Login</button>
-                    </div>
-
-                    
-                    </form>
-
-                </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
     </div>
-
-<?php
-    require("../partials/footer.php");
-?>
+</div>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,4 +1,6 @@
-<?php require "head.php"?>
+<?php
+require "head.php"
+?>
 
 <nav class="navbar navbar-expand-md navbar-light bg-light main-menu" style="box-shadow:none">
     <div class="container-fluid">
@@ -14,14 +16,70 @@
 
             <ul class="navbar-nav">
                 <li class="nav-item ml-md-3">
-                    <div class="btn btn-primary ">
-                        <a href="#" class="text-white"> Log In / </a>
+                    <div class="btn btn-primary">
+                        <a href="#" class="text-white" data-toggle="modal" data-target="#login-modal"> Log In / </a>
+                        <!-- Modal login -->
+                        <div class="modal fade " id="login-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content  w-100px">
+
+                                    <div class="modal-body d-flex">
+
+                                        <div class="col-md-6 col-lg-5 d-none d-md-block ">
+                                            <img src="https://images-na.ssl-images-amazon.com/images/I/6138vGyCo2L._AC_UL900_SR615,900_.jpg" alt="login form" class="img-fluid" />
+                                        </div>
+                                        <div class="col-md-6 col-lg-7 ">
+                                            <div class="card-body p-4 p-lg-5 text-white">
+
+                                                <form>
+
+                                                    <div class="d-flex  mb-3 pb-1">
+                                                        <h3>Sign In</h3>
+                                                    </div>
+
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start" >Email address</label>
+                                                        <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                                    </div>
+
+                                                    <div class="form-outline mb-4">
+                                                        <label class="form-label d-flex justify-content-start " >Password</label>
+                                                        <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                                    </div>
+
+                                                    <!-- Checkbox -->
+                                                    <div class="form-check d-flex justify-content-start mb-3">
+                                                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                                                        <label class="form-check-label ms-3" for="form1Example3"> Remember me! </label>
+                                                    </div>
+
+                                                    <p class="mb-5 pb-lg-2 text-white d-flex justify-content-start" style="color: #393f81;">Don't have an account? <a href="./sign_up.view.php" style="color: #dc3545;">Sign Up</a></p>
+                                                    <div class="pt-1 mb-4">
+                                                        <button class="form-control form-control-lg text-white border border-danger" style="background-color: rgb(159, 4, 4)" type="button">Login</button>
+                                                    </div>
+
+
+                                                </form>
+
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal login -->
+
                         <a href="" class="text-white"> Register</a>
+                            
+
                     </div>
                 </li>
                 <span class="material-symbols-outlined mt-4 ms-4" data-toggle="modal" data-target="#exampleModal">account_circle </span>
-              
-                <!-- Modal -->
+
+                <!-- Modal manage account-->
                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
@@ -49,7 +107,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- modal -->
+                 <!-- Modal manage account-->
             </ul>
         </div>
 
