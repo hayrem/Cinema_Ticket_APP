20f96358d212192a67aa076ee7e6c524390cd6be
hayya <remhay07@gmial.com>
2023-01-25 13:24:34 +0700

    [CP-34] add movie in movie.view.php file

--- views/movie/movie.view.php
+++ views/movie/movie.view.php
@@ -1,6 +1,83 @@
 <?php
-    require "views/partials/head.php";
-    require "views/partials/nav.php";
-    require "views/partials/footer.php";
+    require "../partials/head.php";
+    require "../partials/nav.php";
 ?>
-<h1>hello</h1>
+
+    <div class="container">
+        <div class="card m-3 d-flex flex-lg-row" >
+            <img src="images/cat.png" class="card-img-top" style="width: 15rem;">
+            <div class="card-body">
+              <h4 class="card-title fw-bold">Push In Boots: The Last Wish</h4>
+              <div class="d-flex align-items-center">
+                <button class="bg-danger border-0 m-3" style="color: white;">HD</button>
+                <button class="border-0 bg-light">8.5  ⭐</button>
+                <button class="border-0 bg-light">2023. 29mn   Genre | Anemation advanture</button>
+              </div>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam nihil tempora repudiandae delectus assumenda blanditiis earum laborum adipisci, sit quae eum? Iure saepe praesentium placeat nostrum, iste adipisci quisquam vitae.</p>
+              <div class="" style="line-height: 10px;">
+                <p>Country : Cambodia</p>
+                <p>Genre : Anemation advanture</p>
+                <p>Release : 2023</p>
+                <p>Date show : 12/02/2023 | 5:00 PM - 7:00 PM</p>
+                <p>Cinema : Cinema Ticket PNC</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+              </div>
+        </div>
+    </div>
+    <h2 class="container-fluid p-2" style="color: white;">Treding</h2>
+    <div class="container-fluid d-flex flex-wrap">
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="30%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="20%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="20%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="20%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="20%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+        <div class="card  m-2" style="width: 15rem;">
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg" width="20%" height="70%" class="card-img-top" >
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">2023 : 2h:10nm</p>
+                <a href="#" class="btn btn-danger">Booking</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php require "views/partials/footer.php"; ?>
+
+
