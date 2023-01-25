5c060a24213ffb738f15a344cbe1587e57cb7ab0
hayya <remhay07@gmial.com>
2023-01-25 13:45:27 +0700

    [CP-46] update code in cinema

--- views/cinema/cinema.view.php
+++ views/cinema/cinema.view.php
@@ -2,16 +2,6 @@
     require "../partials/head.php";
     require "../partials/nav.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cinema</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body class="body bg-dark d-flex flex-column container">
     <div class="card m-3 d-flex flex-lg-row" >
         <img src="../images/cinema.png" class="card-img-top" style="width: 12rem;">
         <div class="card-body">
@@ -39,6 +29,4 @@
           <a href="#" class="btn btn-danger">Show Items</a>
         </div>
     </div>
-</body>
-</html>
 <?php require "../partials/footer.php"; ?>
