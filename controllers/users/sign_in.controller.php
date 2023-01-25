6efeaf7e59396530d31bfd4a28456f748c278c13
NONG PHLOEUT <nongphloeut@NONGs-MacBook-Air.local>
2023-01-26 01:16:26 +0700

    [CP-37] clean code in the views

--- controllers/users/sign_in.controller.php
+++ controllers/users/sign_in.controller.php
@@ -1,76 +1,46 @@
 <?php
-session_start();
-$email = ($_POST['email']);
-$password = ($_POST['password']);
-require ("sign_in.model.php");
-require "../models/post.model.php";
-$user = getUserAccount($email,$password);
+// session_start();
+// // require ("sign_in.model.php");
+// require "../../models/post.model.php";
+// $email = ($_POST['email']);
+// $password = ($_POST['password']);
+// $user = getUserAccount($email,$password);
 
-if ($email===$user[0]['user_name'] && $password===$user[0]['password'])
-{
+// if ($email===$user[0]['user_name'] && $password===$user[0]['password'])
+// {
 
-    if(!empty($_POST["remember"])) 
-    {
-        setcookie ("email",$_POST["email"],time()+ 3600);
-        setcookie ("password",$_POST["password"],time()+ 3600);
-    } else {
-        setcookie("email","");
-        setcookie("password","");
-    };
-}
+//     if(!empty($_POST["remember"])) 
+//     {
+//         setcookie ("email",$_POST["email"],time()+ 3600);
+//         setcookie ("password",$_POST["password"],time()+ 3600);
+//     } else {
+//         setcookie("email","");
+//         setcookie("password","");
+//     };
+// }
 ?>
 <?php 
-// session_start(); 
-// include "db_conn.php";
+session_start(); 
 
-// if (isset($_POST['uname']) && isset($_POST['password'])) {
+require "../../database/database.php";
 
-// 	function validate($data){
-//        $data = trim($data);
-// 	   $data = stripslashes($data);
-// 	   $data = htmlspecialchars($data);
-// 	   return $data;
-// 	}
+if(isset($_POST['submit'])){
 
-// 	$uname = validate($_POST['uname']);
-// 	$pass = validate($_POST['password']);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['password']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
 
-// 	if (empty($uname)) {
-// 		header("Location: index.php?error=User Name is required");
-// 	    exit();
-// 	}else if(empty($pass)){
-//         header("Location: index.php?error=Password is required");
-// 	    exit();
-// 	}else{
-// 		// hashing the password
-//         $pass = md5($pass);
+   $select_users = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
+   $select_users->execute([$email, $pass]);
+   $row = $select_users->fetch(PDO::FETCH_ASSOC);
 
-        
-// 		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+   if($select_users->rowCount() > 0){
+      setcookie('user_id', $row['id'], time() + 60*60*24, '/');
+      header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
 
-// 		$stmt = $conn->prepare($sql);
-// 		$stmt ->execute();
-
-
-// 		if ($stmt->rowCount() === 1) {
-// 			$user = $stmt->fetch();
-//             if ($user['user_name'] === $uname && $user['password'] === $pass) {
-//             	$_SESSION['user_name'] = $user['user_name'];
-//             	$_SESSION['name'] = $user['name'];
-//             	$_SESSION['id'] = $user['id'];
-//             	header("Location: home.php");
-// 		        exit();
-//             }else{
-// 				header("Location: index.php?error=Incorect User name or password");
-// 		        exit();
-// 			}
-// 		}else{
-// 			header("Location: index.php?error=Incorect User name or password");
-// 	        exit();
-// 		}
-// 	}
-	
-// }else{
-// 	header("Location: index.php");
-// 	exit();
-// }
+   echo $message;
+}
--- views/users/sign_in.view.php
+++ views/users/sign_in.view.php
@@ -8,37 +8,35 @@ require "../../controllers/users/sign_in.controller.php";
 
 
 <div class="d-flex justify-content-center align-items-center vh-100 text-white">
-    	
-    	<form class="shadow w-450" 
-    	      action="" 
-    	      method="post">
 
-    		<h4 class="display-4  fs-1">LOGIN</h4><br>
+	<form class="shadow w-450" action="" method="post">
 
-		  <div class="mb-3">
-		    <label class="form-label">Email</label>
-		    <input type="text" 
-		           class="form-control"
-		           name="email"
-		           value="">
-		  </div>
+		<h4 class="display-4  fs-1">LOGIN</h4><br>
 
-		  <div class="mb-3">
-		    <label class="form-label">Password</label>
-		    <input type="password" 
-		           class="form-control"
-		           name="pass">
-		  </div>
-		  
-          <div class="d-flex">
-				<label class="form-label">Don't have an Account</label>
-                <a href="sign_up.view.php" class="link-secondary ms-2 text-primary">Sign Up</a>
-            </div>
-		  <div class="contain-btn">
-              <button type="submit" class="btn btn-danger">Login</button>
-          </div>
-		</form>
-    </div>
+		<div class="mb-3">
+			<label class="form-label">Email</label>
+			<input type="email" class="form-control" name="email" value="">
+		</div>
+
+		<div class="mb-3">
+			<label class="form-label">Password</label>
+			<input type="password" class="form-control" name="pass">
+		</div>
+		<div class="form-check mb-4">
+			<input class="form-check-input " type="checkbox" value="" id="remember-me" checked>
+			<label class="form-check-label" for="flexCheckChecked">
+				Remember me
+			</label>
+		</div>
+		<div class="d-flex">
+			<label class="form-label">Don't have an Account</label>
+			<a href="sign_up.view.php" class="link-secondary ms-2 text-primary">Sign Up</a>
+		</div>
+		<div class="contain-btn">
+			<button type="submit" class="btn btn-danger">Login</button>
+		</div>
+	</form>
+</div>
 
 <?php
 require "../partials/footer.php";
