dd275812cab71ddf964ed8133a302d0bcf215629
sreyrea <hansreyrea2222@gmail.com>
2023-01-25 09:55:29 +0700

    Merge branch 'main' of https://github.com/hayrem/Cinema_Ticket_APP into create_account

--- controllers/home/index.controller.php
+++ controllers/home/index.controller.php
@@ -3,7 +3,7 @@
 $heading = "Home page";
 
 require "models/post.model.php";
-
+require "models/list_show.php";
 // $posts = getPosts();
 
 require "views/home/index.view.php";
--- models/list_show.model.php
+++ models/list_show.model.php
@@ -1,11 +1,18 @@
 <?php
-require("../database/database.php");
+require("database/database.php") ;
 function getMovie() : array
 {
     global $connection;
-    $statement = $connection->prepare("select title,released, duration from movies");
+    $statement = $connection->prepare("select title,released, duration,image from movies");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 $shows=getMovie();
+foreach( $shows as $show )
+{
+    echo $show["title"];
+    echo $show["released"];
+    echo $show["duration"];
+    echo $show["image"];
+}
 ?>
