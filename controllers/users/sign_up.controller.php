226ccd2f643b3477c9662f54ce99404b14018440
Student <NongPhloeut>
2023-01-26 19:09:14 +0700

    [CP-53] clean code in home page

--- controllers/users/sign_up.controller.php
+++ controllers/users/sign_up.controller.php
@@ -3,84 +3,95 @@
 require "models/post.model.php";
 require "views/users/sign_up.view.php";
 
-$name_err="";
-$email_err="";
-$password_err="";
-$comfirm_password_err="";
-
-
-// php form data validation function 
-function testInput($data): string
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(empty(['username'])){
+        // echo "hello nothing";
+        print_r (['username']);
+    }else{
+        echo "hello have you";
+    }
 }
-if ($_SERVER["REQUEST_METHOD"] === "POST")
 
-{   
-    // require database connection
-    require "database/database.php";
-
-    $name = testInput($_POST["username"]);
-    $password = testInput($_POST["password"]);
-    $comfirm_password = testInput($_POST["comfirm-password"]);
-    $email = ($_POST["email"]);
+// $name_err="";
+// $email_err="";
+// $password_err="";
+// $comfirm_password_err="";
 
 
-    if (empty($name))
-    {
-        $name_err = 'Please enter you username';
+// // php form data validation function 
+// function testInput($data): string
+// {
+//   $data = trim($data);
+//   $data = stripslashes($data);
+//   $data = htmlspecialchars($data);
+//   return $data;
 
-    }
-    elseif(!preg_match("/^[a-zA-Z\d]+$/",$name ))
-    {
-        $name_err = 'Please enter your username more that 5 letters and least that 20 letters';
+// }
+// if ($_SERVER["REQUEST_METHOD"] === "POST")
+
+// {   
+//     // require database connection
+//     require "database/database.php";
+
+//     $name = testInput($_POST["username"]);
+//     $password = testInput($_POST["password"]);
+//     $comfirm_password = testInput($_POST["comfirm-password"]);
+//     $email = ($_POST["email"]);
 
 
-    }
-    if (!preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/",$email))
-    {
-        $email_err = "Invalid format and please re-enter valid email"; 
-    }
-    elseif(empty($email))
-    {
-        $email_err = " Please enter your email";
-    }
-    if(empty($password))
-    {
-        $password_err = "Please enter your passowrd";
-    }
-    elseif(!preg_match("/^[a-zA-Z\d]{8,50}+$/" ,$password))
-    {
-        $password_err = 'Please enter your password more that 8 cheraters';
-    }
-    if (empty($comfirm_password)) {
-        $comfirm_password_err = 'Please enter you ';
-    }
-    if ($password != $comfirm_password){
-        $password_err="Password doesn't match";
-        $comfirm_password_err="Password doesn't match";
-    }
-    if ($name_err==="" && $password_err==='' && $email_err===''  && $comfirm_password_err==='')
-    {
+//     if (empty($name))
+//     {
+//         $name_err = 'Please enter you username';
+
+//     }
+//     elseif(!preg_match("/^[a-zA-Z\d]+$/",$name ))
+//     {
+//         $name_err = 'Please enter your username more that 5 letters and least that 20 letters';
+
+
+//     }
+//     if (!preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/",$email))
+//     {
+//         $email_err = "Invalid format and please re-enter valid email"; 
+//     }
+//     elseif(empty($email))
+//     {
+//         $email_err = " Please enter your email";
+//     }
+//     if(empty($password))
+//     {
+//         $password_err = "Please enter your passowrd";
+//     }
+//     elseif(!preg_match("/^[a-zA-Z\d]{8,50}+$/" ,$password))
+//     {
+//         $password_err = 'Please enter your password more that 8 cheraters';
+//     }
+//     if (empty($comfirm_password)) {
+//         $comfirm_password_err = 'Please enter you ';
+//     }
+//     if ($password != $comfirm_password){
+//         $password_err="Password doesn't match";
+//         $comfirm_password_err="Password doesn't match";
+//     }
+//     if ($name_err==="" && $password_err==='' && $email_err===''  && $comfirm_password_err==='')
+//     {
+
+//         echo "hello world";
        //incript password
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        // insert data into database
-    	$sql = "INSERT INTO users(user_name, email, password,confirm_password) 
-    	        VALUES(?,?,?,?)";
-    	$stmt = $connection->prepare($sql);
-    	$stmt->execute([$name, $email, $password,$comfirm_password]);
+        // $password = password_hash($password, PASSWORD_DEFAULT);
+        // // insert data into database
+    	// $sql = "INSERT INTO users(user_name, email, password,confirm_password) 
+    	//         VALUES(?,?,?,?)";
+    	// $stmt = $connection->prepare($sql);
+    	// $stmt->execute([$name, $email, $password,$comfirm_password]);
 
-    	header("Location:/?success=Your account has been created successfully");
-	    exit;
+    	// header("Location:/?success=Your account has been created successfully");
+	    // exit;
         // header("location: /");
 
 
-    }
-}
+//     }
+// }
 ?>
 
 <?php
