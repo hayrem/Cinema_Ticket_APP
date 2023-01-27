32460321aa77e0719c11e9d01a14ad4facaedc6b
Student <NongPhloeut>
2023-01-27 11:17:50 +0700

    fix conflex code

--- database/database.php
+++ database/database.php
@@ -9,10 +9,3 @@ $password = "";
 
 $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
 $connection = new PDO($dsn, $username, $password);
-
-// try {
-//     $connection = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-//     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }catch(PDOException $e){
-//   echo "Connection failed : ". $e->getMessage();
-// }
