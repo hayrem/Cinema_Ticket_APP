3bebc3caf12e5530dde5b905b5535b622da1f058
animeocean <ahseangeng777@gmail.com>
2023-01-27 08:38:21 +0700

    [CP-40] create search.control file and search function

--- controllers/home/index.controller.php
+++ controllers/home/index.controller.php
@@ -2,15 +2,12 @@
 // $heading = "Home page";
 
 require "models/list_show.model.php";
+require "controllers/search/search.controller.php";
 
 $heading = "Home page";
 
 require "models/post.model.php";
 // $posts = getPosts();
-require "views/home/index.view.php";
-
-
-
 
 
 ?>
@@ -19,25 +16,6 @@ require "views/home/index.view.php";
 
 require "database/database.php";
 
-if (isset($_POST['input'])) {
-    $input  = $_POST['input'];
-    $query  = "select * form cinema_ticket_app where title like '{$input}%' or released like '{$input}%'
-    OR country like '{$input}%' OR language like '{$input}%'";
-    $result = $connection->query($query);
-    $rows = $result->fetchAll();
 
-    if (!empty($result)) { 
-        foreach ($rows as $row) {
-            $id  = $row['id'];
-            $title = $row['title'];
-            $released = $row['released'];
-            $country = $row['country'];
-            $language = $row['language'];
-        }
+require "views/home/index.view.php";
 
-        echo $id;
-          
-    } else {
-        echo "<h6 class='text-danger text-ceter mt-3 '> no results found</h6>";
-    }
-}
