60bbe6b158d19cc1b88af469c91fa8a2b2bc6218
animeocean <ahseangeng777@gmail.com>
2023-01-27 08:49:45 +0700

    [CP-41] edite list_show.model for search controller

--- models/list_show.model.php
+++ models/list_show.model.php
@@ -1,11 +1,10 @@
 <?php
 require("database/database.php");
-function getMovie() : array
+function getMovie($search) : array
 {
     global $connection;
-    $statement = $connection->prepare("select title,released,image,duration from movies");
+    $statement = $connection->prepare("select title,released,image,duration from movies where title like '%{$search}%'");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
-$shows=getMovie();
 ?>
