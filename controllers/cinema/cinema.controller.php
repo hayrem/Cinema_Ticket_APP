faee504e16f7f4c7db56a3006fddf05cc15913cd
Sreymom832 <sreymom.chhum@student.passerellesnumeriques.org>
2023-01-27 09:06:41 +0700

    [edite cinema]

--- controllers/cinema/cinema.controller.php
+++ controllers/cinema/cinema.controller.php
@@ -1,3 +1,9 @@
+
 <?php 
+    require "views/partials/nav.php";
+    require "views/partials/head.php";
+
     require "views/cinema/cinema.view.php";
+
+    require "views/partials/footer.php";
 ?>
