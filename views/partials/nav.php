a1f7790c4e24ff21fd7d45466a8b18c8292fcaca
Student <NongPhloeut>
2023-02-04 20:14:43 +0700

    [CP-100] clean code

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -7,7 +7,13 @@
 
         <div class="collapse navbar-collapse p-0">
             <form class="form-inline mx-auto d-flex " method='post' name='fsearch'>
-                <input class="form-control " id="live_search" type="search" placeholder="Search for products..." aria-label="Search" name='search'>
+                <input class="form-control " 
+                id="live_search" 
+                type="search" 
+                placeholder="Search for products..." 
+                aria-label="Search" 
+                name='search'
+                value="<?= (isset($_POST['search'])) ? $_POST['search'] : "" ?>">
                 <button style="border: solid white 1px;" class="btn btn-success "  type="submit"><span class="material-symbols-outlined" value='search'>search</span></i></button>
             </form>
 
