b25ab99d63157a267a870c5ee371f47bd2d31cf1
sreyrea <hansreyrea2222@gmail.com>
2023-02-04 19:53:18 +0700

    [CP-121] udate detail hall

--- views/hall/hall.view.php
+++ views/hall/hall.view.php
@@ -1,24 +1,15 @@
 <?php
 ?>
 <div class = "d-flex ms-4">
-  <div class="head mt-4">
-      <div class="show">
-        <img src="https://wallpapercave.com/wp/wp11380131.jpg">
-        <img src="https://wallpapercave.com/wp/wp11380133.jpg">
-        <img src="https://www.lunchbox-productions.com/wp-content/uploads/2021/12/New-God-Nezha-Reborn-2021.jpg">
-        <img src="https://lmanime.com/wp-content/uploads/2022/08/0081q0kXgy1gsxvub5ho3j30u01hcdpz-768x1365.jpg">
-        <img src="https://i0.wp.com/luciferdonghua.in/wp-content/uploads/2021/12/swallowed-star-season-2-lucifer-donghua.webp">
-        <img src="https://wallpapercave.com/wp/wp11380133.jpg">
-        <img src="https://cdn.myanimelist.net/images/anime/1039/126194.jpg">
-      </div>
-      <div class="text-white mt-4">
-        <h1>PP CINEMA</h1>
-        <p>Street 1003, Phnom Penh (Aeon Mall Sen Sok) Aeon Sen Sok</p>
-      </div>
-      <div style="width:100%;">
-        <img src="https://filmfare.wwmindia.com/content/2020/may/multiplex41589991541.jpg" alt="" style="width:100%;">
-      </div>
+  <div class="head mt-2">
+    <div class="text-white ">
+      <h1>PP CINEMA</h1>
+      <p>Street 1003, Phnom Penh (Aeon Mall Sen Sok) Aeon Sen Sok</p>
     </div>
+    <div style="width:100%;">
+      <img src="https://filmfare.wwmindia.com/content/2020/may/multiplex41589991541.jpg" alt="" style="width:100%;">
+    </div>
+  </div>
   <div class="body d-flex flex-column container ">
     <?php
       foreach ( $halls as $hall):
--- views/partials/head.php
+++ views/partials/head.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <!-- style css link -->
     <link rel="stylesheet" href="../../views/css/main.css">
-    <link rel="stylesheet" href="../../views/css/hall.css">
 </head>
 <body>
     
