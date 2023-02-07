50b85c050db7afbcafeea6c0e9e8f44f54f4bfe0
hayya <remhay07@gmial.com>
2023-02-07 15:21:04 +0700

    [CP-120] update contact us page

--- views/contact_us/contact_us.view.php
+++ views/contact_us/contact_us.view.php
@@ -1,5 +1,5 @@
 
-<div class="head_contact bg-dark">
+<div class="head_contact" style="opacity:0.6; ">
     <div class="text d-flex justify-content-center align-items-center">
         <h1 class="text-white">Contact Us</h1>
     </div>
@@ -70,6 +70,7 @@
             </div>
         </div>
     </form>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3909.013834999073!2d104.88089491412248!3d11.550865047581027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951add5e2cd81%3A0x171e0b69c7c6f7ba!2sPasserelles%20num%C3%A9riques%20Cambodia%20(PNC)!5e0!3m2!1sen!2skh!4v1675753940359!5m2!1sen!2skh" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3909.013834999073!2d104.88089491412248!3d11.550865047581027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951add5e2cd81%3A0x171e0b69c7c6f7ba!2sPasserelles%20num%C3%A9riques%20Cambodia%20(PNC)!5e0!3m2!1sen!2skh!4v1675753940359!5m2!1sen!2skh" width="100%" height="350" style="border:0; margin:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 <script src="../../views/js/main.js"></script>
+<?php require 'views/partials/footer.php' ?>
