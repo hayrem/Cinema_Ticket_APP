5135c381a0f4066bb5b505c111e41ffe3e6bd239
sreyrea <hansreyrea2222@gmail.com>
2023-02-09 13:21:27 +0700

    [CP-83] update controller

--- controllers/seller/setting_movie.controller.php
+++ controllers/seller/setting_movie.controller.php
@@ -1,10 +1,10 @@
 <?php
-echo 'sdasdfasdf';
+echo 'Hello World';
 require "views/partials/head.php";
 require "models/delete_show.model.php";
 $sellerShows = sellerShowMovie() ;
 require("views/seller/setting_movie.view.php");
-$id = $_POST['movie_id'];
 if (isset($_GET['submit'])){
+    $id = $_POST['movie_id'];
     $delete = deleteShow($id);
 }
