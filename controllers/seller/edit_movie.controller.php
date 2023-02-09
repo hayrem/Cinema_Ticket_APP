793a2350896da32fdbd3b7656ef1538548b8c203
SREYTOUCH.THOR <THOR.SREYTOUCH@PNC.LAN>
2023-02-09 16:45:11 +0700

    [CP-86] Edit file edit.

--- controllers/seller/edit_movie.controller.php
+++ controllers/seller/edit_movie.controller.php
@@ -1,16 +1,21 @@
 <?php
 
 require "models/seller_list_show.model.php";
+// require "models/list_show.model.php";
+require "models/delete_show.model.php";
 
 read_seller_edit($connection);
 
-    $movie_id = $_GET["movie_id"] ? $_GET["movie_id"] : null;
+    $movie_id = $_GET["id"] ? $_GET["id"] : null;
 
-    if (isset($movie_id)){
+    if (isset($movie_id)):
         $statement = $connection->prepare('select * from movies where movie_id = :movie_id');
         $statement->execute([':movie_id' => $movie_id]);
         $seller = $statement->fetch();
-    }
+        echo ($movie_id);
+       
+    
+    endif;
 
        
        
