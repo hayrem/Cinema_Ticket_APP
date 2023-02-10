e9a659c874282add093f7134244b362f3be3a415
hayya <remhay07@gmial.com>
2023-02-10 10:32:21 +0700

    [CP-96] update ticket page

--- views/ticket/ticket.php
+++ views/ticket/ticket.php
@@ -1,48 +1,51 @@
 
 <h1 class="text-white " style="margin:40px; border-radius:10px 10px 10px 10px ; background:#DC2026;; width:18%; text-align: center;" >View ticket</h1>
 
-<div class="container bg-danger">
+<div class="container bg-danger p-4">
 
-<div class="ticket">
-	<div class="left">
-		<div class="image card-ticket ">
-			<img src=" " alt="" srcset="">
+	<div class="ticket">
+		<div class="left">
+			<div class="image card-ticket ">
+				<img src=" " alt="" srcset="">
+			</div>
+			<div class="ticket-info">
+				<p class="date">
+					<span> </span>
+				</p>
+				<div class="show-name">
+					<h1> </h1>
+					<h2>  </h2>
+				</div>
+				<div class="time">
+					<p>  <span>TO</span></p>
+				</div>
+				<p class="location">
+					<span>Street 371, Sangkat Obek Kaorm, Khan Sen Sok Phnom Penh</span>
+				</p>
+			</div>
 		</div>
-		<div class="ticket-info">
-			<p class="date">
-				<span> </span>
-			</p>
-			<div class="show-name">
-				<h1> </h1>
-				<h2>  </h2>
-			</div>
-			<div class="time">
-				<p>  <span>TO</span></p>
-			</div>
-			<p class="location">
-				<span>Street 371, Sangkat Obek Kaorm, Khan Sen Sok Phnom Penh</span>
-			</p>
-		</div>
-	</div>
-	<div class="right">
-		<div class="right-info-container">
-			<div class="show-name">
-				<h1></h1>
-			</div>
-			<div class="time">
-				<p> <span>TO</span></p>
-			</div>
-			
-			<p class="seat-number">SEAT
+		<div class="right">
+			<div class="right-info-container">
+				<div class="show-name">
+					<h1></h1>
+				</div>
+				<div class="time">
+					<p> <span>TO</span></p>
+				</div>
 				
-			</p>
+				<p class="seat-number">SEAT
+					
+				</p>
+			</div>
 		</div>
 	</div>
-</div>
 
-<!-- <button type="button" class="btn btn-save text-white">Save</button> -->
 
-	
+
+	<div class="print d-flex justify-content-between">
+		<button type="button" class="btn btn-save text-white">Save</button>
+		<button type="button" class="btn btn-save text-white">Print</button>
+	</div>
 </div>
 <?php
 require "views/partials/footer.php";
