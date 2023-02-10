380e6e58794b9c140d69630afac7cc177357213a
sreyrea <hansreyrea2222@gmail.com>
2023-02-10 10:14:41 +0700

    [CP-96] fix bug

--- controllers/ticket/ticket.controller.php
+++ controllers/ticket/ticket.controller.php
@@ -2,8 +2,12 @@
     require "views/partials/head.php";
     require "views/partials/nav.php";
     require "models/ticket.model.php";
+    
     $deading = "cinema";
     $tickets = showTicket();
+    
+    print_r ($tickets);
+
     require "views/ticket/ticket.php";
 
 ?>
--- views/ticket/ticket.php
+++ views/ticket/ticket.php
@@ -1,22 +1,23 @@
+
 <h1 class="text-white " style="margin:40px; border-radius:10px 10px 10px 10px ; background:#DC2026;; width:18%; text-align: center;" >View ticket</h1>
-<?php
-foreach ($tickets as $ticket):
-?>
+
+<div class="container bg-danger">
+
 <div class="ticket">
 	<div class="left">
 		<div class="image card-ticket ">
-			<img src="<?php echo $ticket["image"]?> " alt="" srcset="">
+			<img src=" " alt="" srcset="">
 		</div>
 		<div class="ticket-info">
 			<p class="date">
-				<span> <?php echo $ticket["name"]." ".$ticket["date"] ?></span>
+				<span> </span>
 			</p>
 			<div class="show-name">
-				<h1> <?php echo $ticket["first_name"]." ".$ticket["last_name"] ?></h1>
-				<h2> <?php echo $ticket["title"] ?> </h2>
+				<h1> </h1>
+				<h2>  </h2>
 			</div>
 			<div class="time">
-				<p> <?php echo $ticket["time_start"]."s"." "."PM"?> <span>TO</span> <?php echo $ticket["time_end"]."s"." "."PM"?></p>
+				<p>  <span>TO</span></p>
 			</div>
 			<p class="location">
 				<span>Street 371, Sangkat Obek Kaorm, Khan Sen Sok Phnom Penh</span>
@@ -26,20 +27,23 @@ foreach ($tickets as $ticket):
 	<div class="right">
 		<div class="right-info-container">
 			<div class="show-name">
-				<h1><?php echo $ticket["first_name"]." ".$ticket["last_name"] ?></h1>
+				<h1></h1>
 			</div>
 			<div class="time">
-				<p> <?php echo $ticket["time_start"]."s"." "."PM"?> <span>TO</span><?php echo $ticket["time_end"]."s"." "."PM" ?></p>
+				<p> <span>TO</span></p>
 			</div>
 			
 			<p class="seat-number">SEAT
-				<?php echo "seat number" ?>
+				
 			</p>
 		</div>
 	</div>
 </div>
-<button type="button" class="btn btn-save text-white">Save</button>
-<?php endforeach; ?>
+
+<!-- <button type="button" class="btn btn-save text-white">Save</button> -->
+
+	
+</div>
 <?php
 require "views/partials/footer.php";
 
