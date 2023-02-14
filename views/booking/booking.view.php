354177c10e3a883158559dc42964c6c486c4e634
animeocean <ahseangeng777@gmail.com>
2023-02-14 15:29:40 +0700

    [CP-133] fix some bug

--- views/booking/booking.view.php
+++ views/booking/booking.view.php
@@ -43,7 +43,7 @@
     </div>
 
     <div class="info-form">
-        <form action="" class="w-75" method='POST'>
+        <form action="" class="w-75" method='POST' onsubmit='return false'>
             <span class="fs-4 mb-4">Show time detail</span>
             <div class="mb-3 mt-4">
                 <label class="form-label fs-5">Hall :</label>
@@ -63,38 +63,37 @@
 		    </div>
             <span class="fs-4 mb-4">Customer info</span>
             <div class="row g-3 mb-3 mt-3">
-                <div class="col">
+                <div class="col" id='input-firstname'>
                     <label class="form-label">First name</label>
-                    <input type="text" class="form-control" placeholder="First name" aria-label="First name" name="firstName" value="<?= (isset($_POST['firstName'])) ? $_POST['firstName'] : "" ?>">
-                    <span style="color:red;"><?= (isset($messageError['firstName'])) ? $messageError['firstName'] : "" ?></span>
+                    <input type="text" class="form-control input-firstname" placeholder="First name" aria-label="First name" name="firstName" value="<?= (isset($_POST['firstName'])) ? $_POST['firstName'] : "" ?>">
+                    <span style="color:red;" id="firstname"></span>
                 </div>
-                <div class="col">
+                <div class="col" id='input-lastname'>
                     <label class="form-label">Last name</label>
-                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" name="lastName" value="<?= (isset($_POST['lastName'])) ? $_POST['lastName'] : "" ?>">
-                    <span style="color:red;"><?= (isset($messageError['lastName'])) ? $messageError['lastName'] : "" ?></span>
+                    <input type="text" class="form-control input-lastname" placeholder="Last name" aria-label="Last name" name="lastName" value="<?= (isset($_POST['lastName'])) ? $_POST['lastName'] : "" ?>">
+                    <span style="color:red;" id="lastname"></span>
                 </div>
             </div>
-            <div class="mb-3">
+            <div class="mb-3" id='input-email'>
                 <label class="form-label">Email</label>
                 <input type="email" 
-                class="form-control"
+                class="form-control input-email"
                 name="email"
                 placeholder="Enter you email address" 
                 value="<?= (isset($_POST['email'])) ? $_POST['email'] : "" ?>">
-                <span style="color:red;"><?= (isset($messageError['email'])) ?
-                $messageError['email'] : "" ?></span>
+                <span style="color:red;" id="email"></span>
 		    </div>
-            <div class="mb-3">
-                <label class="form-label">Phone number</label>
+            <div class="mb-3" id='input-phonenumber'>
+                <label class="form-label ">Phone number</label>
                 <input type="number"
-                class="form-control" 
+                class="form-control input-phonenumber" 
                 name="number" 
                 placeholder="Enter your phone number"
                 value="<?= (isset($_POST['number'])) ? $_POST['number'] : "" ?>">
-                <span style="color:red;"><?= (isset($messageError['number'])) ? $messageError['number'] : "" ?></span>
+                <span style="color:red;" id="phonenumber"></span>
             </div class="mb-3">
             <!-- <button class="popup-button btn btn-primary w-100 mt-4">PAYMENT</button> -->
-            <input class="btn btn-primary popup-button w-100 mt-4 p-2" type="submit" name="payment" value="PAYMENT" href=""/>
+            <input class="btn btn-primary popup-button w-100 mt-4 p-2" type="submit" name="payment" value="PAYMENT" href="" onclick="validationCustomerInfo(); "/>
             
         </form>
         
@@ -118,12 +117,12 @@ price.textContent = totalPrice*seatPrice + '$';
 }
 </script>
 <?php
-    if (isset($_POST['payment']))
-    {{if (isset($messageError)){
-        if (empty($messageError)){
-            require 'views/booking/payment.view.php';
+//     if (isset($_POST['payment']))
+//     {{if (isset($messageError)){
+//         if (empty($messageError)){
+//             require 'views/booking/payment.view.php';
             
-$messageError = [];
+// $messageError = [];
 // function validation($data): string
 // {
 //     $data = trim($data);
@@ -132,42 +131,130 @@ $messageError = [];
 //     return $data;
     
 // }
-if (isset($_POST['payment-btn']))
-{if($_SERVER['REQUEST_METHOD'] === 'POST')
-{
+// if (isset($_POST['payment-btn']))
+// {if($_SERVER['REQUEST_METHOD'] === 'POST')
+// {
     
-    $cardName = validation($_POST["card-name"]);
-    $cardNumber = validation($_POST["card-number"]);
-    $expired = validation($_POST["expired"]);
-    $cvv = validation($_POST["cvv"]);
-    if(empty($cardName)){
-        $messageError["firstName"] = "Please enter your card name";
-    }elseif(!preg_match("/^[a-zA-Z\d]+$/",$cardName))
-    {
-        $messageError["card-name"] = 'Card name most more that 5 letters and least that 20 letters';
-    }
-    if(empty($cardNumber)){
-        $messageError["card-number"] = "Please enter your card number";
-    }elseif(!preg_match("/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/",$cardNumber))
-    {
-        $messageError["card-number"] = 'Card number must be 16 digits';
-    }
-    if(empty($expired))
-    {
-        $messageError["expired"] = "Please enter your expired";
-    } 
-    elseif (!preg_match("/^(0?[1-9]|1[0-2])([0-9]{2})$/",$expired))
-    {
-        $messageError["expired"] = "Expired must be only 4 digits"; 
-    }
-    if (empty($cvv)){
-        $messageError['cvv'] = 'Please enter your cvv';
-    }
-    elseif (!preg_match("/^([1-9]\/|[2-9])$/",$cvv)){
-        $messageError['cvv'] = 'CVV must be 3 digits';
-    }
-}}
+//     $cardName = validation($_POST["card-name"]);
+//     $cardNumber = validation($_POST["card-number"]);
+//     $expired = validation($_POST["expired"]);
+//     $cvv = validation($_POST["cvv"]);
+//     if(empty($cardName)){
+//         $messageError["firstName"] = "Please enter your card name";
+//     }elseif(!preg_match("/^[a-zA-Z\d]+$/",$cardName))
+//     {
+//         $messageError["card-name"] = 'Card name most more that 5 letters and least that 20 letters';
+//     }
+//     if(empty($cardNumber)){
+//         $messageError["card-number"] = "Please enter your card number";
+//     }elseif(!preg_match("/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/",$cardNumber))
+//     {
+//         $messageError["card-number"] = 'Card number must be 16 digits';
+//     }
+//     if(empty($expired))
+//     {
+//         $messageError["expired"] = "Please enter your expired";
+//     } 
+//     elseif (!preg_match("/^(0?[1-9]|1[0-2])([0-9]{2})$/",$expired))
+//     {
+//         $messageError["expired"] = "Expired must be only 4 digits"; 
+//     }
+//     if (empty($cvv)){
+//         $messageError['cvv'] = 'Please enter your cvv';
+//     }
+//     elseif (!preg_match("/^([1-9]\/|[2-9])$/",$cvv)){
+//         $messageError['cvv'] = 'CVV must be 3 digits';
+//     }
+// }}
 
+//         }
+//     }};};
+// ?>
+
+<script>
+    const firstname = document.querySelector(".input-firstname").value;
+    const lastname = document.querySelector(".input-lastname").value;
+    const email = document.querySelector(".input-email").value;
+    const phonenumber = document.querySelector(".input-phonenumber").value;
+
+    let inputFirstName= document.querySelector("#input-firstname");
+    let inputLastName= document.querySelector("#input-lastname");
+    let inputPhoneNumber= document.querySelector("#input-phonenumber");
+    let inputEmail= document.querySelector("#input-email");
+
+    let spanfirstname=document.querySelector("#firstname");
+    let spanlistname=document.querySelector("#lastname");
+    let spanemail=document.querySelector("#email");
+    let spanphonenumber=document.querySelector("#phonenumber");
+
+    let regex = 
+    [
+        {name: "/^[a-zA-Z\d]+$/"},
+        {email: "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/"},
+        {phoneNumber: "/^[0].[0-9]{7,8}+$/"}
+    ]
+    let listInput = [
+        firstname,
+        lastname,
+        email,
+        phonenumber
+    ]
+    let inputForm = [
+        inputFirstName,
+        inputLastName,
+        inputPhoneNumber,
+        inputEmail
+    ]
+    let spanList=[
+        spanfirstname,
+        spanlistname,
+        spanemail,
+        spanphonenumber
+    ]
+    let messageErrorList=[
+        'Please enter your Name',
+        'Username most more that 5 letters and least that 20 letters',
+        'Please enter your email',
+        'Invalid format and please re-enter valid email',
+        'Please enter your phone number',
+        'Zero mush in the front and Number mush be 9 or 10 number'
+
+        
+    ]
+    function validationInput(value){
+        if (!empty(value)){
+            let test = regex.test(value)
+            if (test){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return 'empty'
         }
-    }};};
-?>
+        
+    }
+
+    function validationCustomerInfo(){
+        let index = 0
+        for (let value in listInput ){
+            let counterror = 0
+            let key = validationInput(listInput[value])
+            if (key === 'empty'){
+                counterror+=1
+                spanList[$value].textContent = messageErrorList[index]
+            }
+            // elseif (key === false){
+            //     spanList[$value].textContent = '';
+            else{
+                index+=1
+                counterror+=1
+                spanList[$value].textContent = messageErrorList[index]
+                
+            }
+            inputForm[$value].appendChild(spanList[value]);
+            index+=1
+        }
+    }
+
+</script>
