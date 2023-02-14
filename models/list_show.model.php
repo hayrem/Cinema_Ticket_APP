5adbd74bb9627b6c37735a940ebc3729b2ce4f87
sreyrea <hansreyrea2222@gmail.com>
2023-02-14 15:50:22 +0700

    [CP-133
    ] fix bug

--- models/list_show.model.php
+++ models/list_show.model.php
@@ -25,7 +25,7 @@ function getDetailMovie(int $getID) : array
     global $connection;
     $statement = $connection->prepare("SELECT * FROM movies 
     INNER JOIN shows ON movies.movie_id = shows.show_id 
-    INNER JOIN cinema_halls ON shows.cinema_hall_id = cinema_halls.cinema_hall_id
+    INNER JOIN halls ON shows.hall_id = halls.hall_id
     WHERE movies.movie_id = :movie_id");
     $statement->execute([':movie_id'=> $getID]);
     return $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -36,7 +36,7 @@ function getDetailMovie(int $getID) : array
 function getDetahall() : array
 {
     global $connection;
-    $statement = $connection->prepare("SELECT * FROM cinema_halls");
+    $statement = $connection->prepare("SELECT * FROM halls");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
  
@@ -58,11 +58,11 @@ function notListShow(int $id) : array
 function hallShow(int $ID) : array
 {
     global $connection;
-    $statement = $connection->prepare("SELECT * FROM cinema_halls 
-    INNER JOIN shows ON shows.cinema_hall_id = cinema_halls.cinema_hall_id  
+    $statement = $connection->prepare("SELECT * FROM halls 
+    INNER JOIN shows ON shows.hall_id =halls.hall_id  
     INNER JOIN movies ON shows.show_id = movies.movie_id
-    WHERE cinema_halls.cinema_hall_id = :cinema_hall_id");
-    $statement->execute([':cinema_hall_id'=> $ID]);
+    WHERE halls.hall_id = :hall_id");
+    $statement->execute([':hall_id'=> $ID]);
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 };
 // function get name hall
@@ -70,9 +70,9 @@ function hallShow(int $ID) : array
 function getNameHall(int $ID) : array
 {
     global $connection;
-    $statement = $connection->prepare("SELECT * FROM cinema_halls 
-    WHERE cinema_halls.cinema_hall_id = :cinema_hall_id");
-    $statement->execute([':cinema_hall_id'=> $ID]);
+    $statement = $connection->prepare("SELECT * FROM halls 
+    WHERE halls.hall_id = :hall_id");
+    $statement->execute([':hall_id'=> $ID]);
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 };
 ?>
