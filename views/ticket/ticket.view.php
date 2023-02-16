b5a2ab8227d6b4dc378d39a512b5fa311b5927b2
hayya <remhay07@gmial.com>
2023-02-16 13:35:28 +0700

    [CP-97] update print ticket

--- views/ticket/ticket.view.php
+++ views/ticket/ticket.view.php
@@ -1,40 +1,46 @@
-<div class="container-movecards mt-4">
-	<div class="row justify-content-center mb-3 " style="width: 100%;border:none">
-		<div class="col-xl-10">
-			<div class="card rounded-3">
-				<div class="card-body p-0">
-					<div class="image-itme row bg-danger">
-						<div class="col-md-3 col-lg-3 col-xl-2 mb-4 mb-lg-0">
-							<div class="bg-image hover-zoom ripple rounded ripple-surface">
-								<img src="../../assets/phnom-penh-cinema-low-resolution-logo-color-on-transparent-background.png" class="w-100" />
-							</div>
-						</div>
-						<div class="col-md-6 col-lg-6 col-xl-6 p-3 bg-dark" style="width:65%;">
-							<div class="col mt-1 mb-0  ">
-								<h5 class="row"></h5>
-								<span class="row mt-2"> Country: </span>
-								<span class="row mt-2">Genre: </span>
-								<span class="row mt-2">Release:</span>
-							</div>
-						</div>
-						<div class="col-md-6 col-lg-2 col-xl-0  p-3">
-							<div class="d-flex flex-row align-items-center mb-1">
-								<h4 class="mb-1 me-1"></h4>
-							</div>
-						</div>
-					</div>
-				</div>
+<div class="ticket mb-4" id="imgItem">
+	<div class="left">
+		<div class="image card-ticket ">
+			<img src="../../assets/PNC cinema.png" alt="" srcset="">
+		</div>
+		<div class="ticket-info">
+			<p class="title">
+				<span>Harry Potter</span>
+			</p>
+			<div class="show-name">
+				<h2> Rem Hay</h2>
 			</div>
+			<div class="date-time">
+				<p> Thursday 16 Feb 2023 / 6:00 PM</p>
+			</div>
+			<p class="location">
+				<span>Street 371, Sangkat Obek Kaorm, Khan Sen Sok Phnom Penh</span>
+			</p>
+		</div>
+	</div>
+	<div class="right">
+		<div class="right-info-container">
+			<div class="show-name">
+				<h1>Harry Potter</h1>
+				<h2>Rem Hay</h2>
+			</div>
+			<div class="date-time">
+				<p>16 Feb 2023 / 6:00 PM</p>
+			</div>
+			
+			<p class="seat-number">SEAT: A1</p>
 		</div>
 	</div>
 </div>
-<div class="container">
-	<div class="print w-100 " >
-		<span>
-			<button onclick="changeToImg()" class="btn btn-primary text-white" style="margin-left: 30px;">Print Ticket <i class="fas fa-print"></i></button>
-			<i class="fas fa-download"></i>
+<div class="container" style="margin-left:9.9rem;">
+	<div class="print" >
+		<span class="mb-4">
+			<button onclick="changeToImg()" class="btn btn-primary text-white" >Print Ticket <i class="fas fa-print"></i></button>
+			<a id="download" class="font-lg download mr-3 text-white ms-3">
+          		<i class="fas fa-download"></i>
+        	</a>
 		</span>
-		<div id="sreenshot" class="card-body sreenshot">
+		<div id="sreenshot" class="card-body sreenshot mt-4" style="width:90%;">
 			<!-- !--  --> 
 		</div>
 	</div>
